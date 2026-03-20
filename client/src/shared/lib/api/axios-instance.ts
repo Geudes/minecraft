15f818cc9d096af7328b4f42143c253e8dbfd646
Nikita <reddragonson@yandex.ru>
2026-03-20 11:40:09 +0300

    Добавил axiosInstance

---
+++ client/src/shared/lib/api/axios-instance.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { authStorage } from "../../../features/auth/models/auht-storage";
+
+const baseURL: string = import.meta.env.VITE_BACK_URL
+
+const axiosInstance = axios.create({
+    baseURL,
+    headers: {
+        'Content-Type': 'application'
+    }
+})
+
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = authStorage.getAccess()
+        if(accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`
+        }
+
+        return config
+    }, (error) => Promise.reject(error)
+)

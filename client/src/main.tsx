bc8ce148aaa588cf0dacb59434d2121bf42d16c8
IREK <irek130407@gmail.com>
2026-03-20 13:25:51 +0300

    Доп

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router'
 import router from './app/router/router'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './shared/queryClient/query-client'
-import type { registerType } from './entities/servres/model/servers-data'
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>

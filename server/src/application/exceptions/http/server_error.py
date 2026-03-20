7c39c8f18999eb54af7ac60dba550b7a06d6c9a4
Nikita <reddragonson@yandex.ru>
2026-03-20 15:05:09 +0300

    delete server

--- server/src/application/exceptions/http/server_error.py
+++
@@ -1,9 +0,0 @@
-from src.application.exceptions.http.base import ExceptionsBase, status
-
-class ServerException(ExceptionsBase):
-    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    detail = "Ошибка на стороне сервера"
-
-class ServiceUnavailableException(ExceptionsBase):
-    status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-    detail = "Сервис временно недоступен"

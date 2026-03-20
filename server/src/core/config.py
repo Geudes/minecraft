e172018656253b40464152995b009de89524a1ce
michida89 <michida89@gmail.com>
2026-03-20 13:48:45 +0300

    Update config.py

---
+++ server/src/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+    DATABASE_URL: str ="sqlite+aiosqlite:///./database.sqlite3"
+    JWT_ALG: str ='HS256'
+    JWT_KEY: str ='JWT_KEY'

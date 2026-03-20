2e0ef5c56fc93cbf287d271e76bde6ee2a8cc22a
michida89 <michida89@gmail.com>
2026-03-20 14:04:05 +0300

    Update utils.py

---
+++ server/src/core/database/utils.py
@@ -0,0 +1,36 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, async_sessionmaker, AsyncSession, AsyncEngine
+)
+
+
+class Database:    
+    engine: AsyncEngine
+    session: AsyncSession
+    
+    def engine(self, 
+        url: str,    
+        echo: bool=False
+    ):
+        self.engine = create_async_engine(
+            url,
+            echo=echo
+        )
+
+    def session(self, autoflush:bool = False):
+        self.session = async_sessionmaker(
+            self.engine,
+            class_=AsyncSession,
+            autoflush=False
+        )
+        
+        
+    async def get_session(self):
+        async with self.session() as session:
+            try:
+                yield session
+                await session.flush()
+            except Exception as e:
+                await session.rollback()
+            finally:
+                await session.close()
+            

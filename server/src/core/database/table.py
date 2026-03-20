debe8ba32f8e04fb008653b04a48507fbf65dc92
vanechek <kn0w.reedz.77@mail.ru>
2026-03-20 13:18:30 +0300

    w

--- server/src/core/database/table.py
+++ server/src/core/database/table.py
@@ -1,15 +1,18 @@
-from uuid import UUID
 from uuid import uuid4
 from datetime import datetime
+from uuid import UUID
+from sqlalchemy import UUID as SQL_UUID, DateTime, String
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, declared_attr
+
 
 class TableSQLBase(DeclarativeBase):
-    id: Mapped[UUID] = mapped_column(UUID, default=uuid4(), unqiue=True, primery_key=True)
+    id: Mapped[UUID] = mapped_column(SQL_UUID, default=uuid4(), unqiue=True, primery_key=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), nuleble=False)
     updated_at: Mapped[datetime] = mapped_column(DateTime, onupdate=datetime.now(), nuleble=True)
 
-    @getattr
-    def __tablename(cls):
-        return f"{cls.__name__.lover()}s"
+    @declared_attr
+    def __tablename__(cls):
+        return f"{cls.__name__.lower()}s"
 
 
 class User(TableSQLBase):
